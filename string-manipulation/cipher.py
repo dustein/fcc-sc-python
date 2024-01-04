88ab4163ce4bad6b8386c16876a1de7c0090634e
dustein <steindu@gmail.com>
2024-01-03 21:07:09 -0300

    step 74

--- string-manipulation/cipher.py
+++ string-manipulation/cipher.py
@@ -1,19 +1,27 @@
 text = 'Hello Zaira'
-shift = 3
+custom_key = 'python'
 
-def caesar(message, offset):
+def vigenere(message, key, direction=1):
     alphabet = 'abcdefghijklmnopqrstuvwxyz'
-    encrypted_text = ''
+    final_message = ''
+    key_index = 0
 
     for char in message.lower():
         if char == ' ':
-            encrypted_text += char
+            #Append space to the message
+            final_message += char
         else:
+            #Find the right key character to encode/decode
+            key_char = key[key_index % len(key)]
+            key_index += 1
+            #Define the offset and the encrypted/decrypted letter
+            offset = alphabet.index(key_char)
             index = alphabet.find(char)
-            new_index = (index + offset) % len(alphabet)
-            encrypted_text += alphabet[new_index]
-    print('encrypted text:', encrypted_text)
-    print('plain text:', message)
+            new_index = (index + offset * direction) % len(alphabet)
+            final_message += alphabet[new_index]
+    return final_message
 
-caesar(text, shift)
-caesar(text, 12)
+encryption = vigenere(text, custom_key)
+decryption = vigenere(encryption, custom_key, -1)
+print(encryption)
+print(decryption)
