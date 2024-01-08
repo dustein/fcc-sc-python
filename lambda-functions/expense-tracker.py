6e7d2c55a70c22c6f5cd37642fa82bf816f98a10
dustein <steindu@gmail.com>
2024-01-08 17:11:06 -0300

    work goin

--- lambda-functions/expense-tracker.py
+++ lambda-functions/expense-tracker.py
@@ -9,7 +9,16 @@ def princ_expenses(expenses):
     print(f'Amount: {expense["amount"]}, Category: {expense["category"]}')
 
 def total_expenses(expenses):
-  pass
+  return sum(map(lambda expense : expense['amount'], expenses))
+
+def filter_expenses_by_category(expenses, category):
+  return filter(lambda expense : expense['category'] == category, expenses)
+
+def main():
+  expenses = []
+
+# teste = lambda x : x * 2
+# print(sum(map(teste, [2, 3, 5, 8])))
 
 print("Expenses: ")
 print(expenses)
