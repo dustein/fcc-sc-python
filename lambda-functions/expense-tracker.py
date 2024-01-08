d25b9b462cf72e6969326bfd6e1c3c5d9ea965d5
dustein <steindu@gmail.com>
2024-01-08 18:13:11 -0300

    finishing work

--- lambda-functions/expense-tracker.py
+++ lambda-functions/expense-tracker.py
@@ -16,7 +16,20 @@ def filter_expenses_by_category(expenses, category):
 
 def main():
   expenses = []
-
+  while True:
+    print('\nExpense Tracker')
+    print('1. Add an expense')
+    print('2. List all expenses')
+    print('3. Show total expenses')
+    print('4. Filter expenses by category')
+    print('5. Exit')
+        
+    choice = input('Enter your choice: ')
+    if choice == '1':
+      amount = float(input('Enter amount: '))
+      category = input('Enter category: ')
+      add_expenses(expenses, amount, category)
+      
 # teste = lambda x : x * 2
 # print(sum(map(teste, [2, 3, 5, 8])))
 
