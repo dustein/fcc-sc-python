019789c3de0e44046cba557c3867890c952c0764
dustein <steindu@gmail.com>
2024-01-08 13:39:19 -0300

    back to lambda

--- lambda-functions/expense-tracker.py
+++ lambda-functions/expense-tracker.py
@@ -5,6 +5,10 @@ expenses = []
 
 
 def princ_expenses(expenses):
+  for expense in expenses:
+    print(f'Amount: {expense["amount"]}, Category: {expense["category"]}')
+
+def total_expenses(expenses):
   pass
 
 print("Expenses: ")
