161ed0ddea4575e709c324604610afca0500c8a6
dustein <steindu@gmail.com>
2024-01-12 17:26:41 -0300

    retomando work

--- minhas_notas/dict_comprehension.py
+++ minhas_notas/dict_comprehension.py
@@ -1,2 +1,23 @@
-# {chave : recebe valor x for elemento in dados iteraveis}
+# {chave : recebe valor x FOR elemento IN dados iteraveis}
 
+dicionario1 = {elemento: elemento * 2 for elemento in range(5)}
+print(dicionario1)
+
+#---------------------------------------------------------------
+
+lista2 = ['Uno', 'Palio', 'Punto']
+dicionario2 = {f'{elemento.lower()}' : f'muda para {elemento.upper()}' for elemento in lista2}
+print(dicionario2)
+
+#---------------------------------------------------------------
+
+dict_carros = { 'Uno' : 1000, 'Palio' : 2000, 'Punto' : 3000}
+dicionario3 = { chave: f'{chave.upper()} vale {valor}' for chave, valor in dict_carros.items() }
+print(dicionario3)
+
+#---------------------------------------------------------------
+
+dicionario4 = { chave: f'{chave.upper()} custa {valor}' for chave, valor in dict_carros.items() if valor >= 2000 }
+print(dicionario4)
+
+#---------------------------------------------------------------
